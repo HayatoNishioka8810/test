ad6d11d3a886ccb476a5704c0f7b5c0695278e1d
HAYATOa@co.jp <HAYATO a@co.jp>
2022-07-03 23:31:58 +0900

    テストだってばよ

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello World! ★★";
     }
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
